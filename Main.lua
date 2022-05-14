8189262cc4f788b8ea2e1f3e7b7ba4a7489e3a04
StrokeThePea <76650942+StrokeThePea@users.noreply.github.com>
2022-05-14 15:44:19 +0200

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,9 @@
+Options = {
+	["Jitteryness"] = Vector3.new(29,0,0), -- Velocity
+	["Type"] = "Raw", -- Raw (Simple), Fling, Bullet
+	["InstantBullet"] = true, -- Type has to be Bullet
+	["R15Method"] = "Align", -- Align,AlignMax Or CFrame
+	["R6Method"] = "Align", -- Align,AlignMax Or CFrame
+	["BonusProperties"] = true -- Net, and other stuff.
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StrokeThePea/CatwareReanimate/main/src/Source.lua"))()
