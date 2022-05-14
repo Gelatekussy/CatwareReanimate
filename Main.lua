c5d7c98a45750fa766a6a8d04050d8aa3289744e
StrokeThePea <76650942+StrokeThePea@users.noreply.github.com>
2022-05-15 01:17:02 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 Options = {
 	["Jitteryness"] = Vector3.new(29,0,0), -- Velocity
-	["Type"] = "Raw", -- Raw (Simple), Fling, Bullet
-	["InstantBullet"] = true, -- Type has to be Bullet
+	["Type"] = "Raw", -- Raw (Simple), Fling, Bullet, Godmode
+	["InstantBullet"] = true, -- Type has to be Bullet or Godmode
 	["R15Method"] = "Align", -- Align,AlignMax Or CFrame
 	["R6Method"] = "Align", -- Align,AlignMax Or CFrame
 	["BonusProperties"] = true -- Net, and other stuff.
