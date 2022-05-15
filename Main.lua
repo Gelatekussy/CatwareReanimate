17ad943d07137dc27a4187cbd4665d4c64718816
StrokeThePea <76650942+StrokeThePea@users.noreply.github.com>
2022-05-15 21:37:03 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,9 +1,15 @@
 Options = {
-	["Jitteryness"] = Vector3.new(29,0,0), -- Velocity
-	["Type"] = "Raw", -- Raw (Simple), Fling, Bullet, Godmode
-	["InstantBullet"] = true, -- Type has to be Bullet or Godmode
+	["Jitteryness"] = Vector3.new(30.5,0,0), -- Velocity
+	["Type"] = "Godmode", -- Raw (Simple), Fling, Bullet, Godmode
+	["InstantBullet"] = {
+		["Bool"] = true, -- Enables it
+		["SmartLock"] = true -- Locks Bullet On Head
+	}, -- Type has to be Bullet/Godmode
 	["R15Method"] = "Align", -- Align,AlignMax Or CFrame
 	["R6Method"] = "Align", -- Align,AlignMax Or CFrame
-	["BonusProperties"] = true -- Net, and other stuff.
+	["BonusProperties"] = true, -- Net, and other stuff.
+	["RigAnimations"] = true, -- Enables Default Animate
+	["LoadLibrary"] = false -- Loads LoadLibrary for scripts that still use it.
 }
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/StrokeThePea/CatwareReanimate/main/src/Source.lua"))()
